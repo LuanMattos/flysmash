ff38e16e81fe8838ee5c412dc526ae8205d660b6
patrick_mattos <patrick_mattos@terceiros.sicredi.com.br>
2021-11-22 12:46:37 -0300

    Limpeza de código no photo-profile

--- src/app/photos/photo-list/photo-profile-form/photo-profile-form.component.ts
+++ src/app/photos/photo-list/photo-profile-form/photo-profile-form.component.ts
@@ -1,16 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
 import { HttpEvent, HttpEventType } from '@angular/common/http';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { finalize } from 'rxjs/operators';
-import { ImageCroppedEvent, base64ToFile, LoadedImage } from 'ngx-image-cropper';
-import Swal from 'sweetalert2';
+import { ImageCroppedEvent } from 'ngx-image-cropper';
 
 
 import { PhotoService } from '../../photo/photo.service';
-import { AlertService } from '../../../shared/alert/alert.service';
 import { User } from '../../../core/user/user';
-import { SecurityCommonsService } from '../../../shared/services/security-commons.service';
 import { HeaderService } from '../../../core/header/header.service';
 import { PostsService } from 'src/app/core/posts/posts.service';
 import { UserService } from 'src/app/core/user/user.service';
@@ -28,28 +24,21 @@ export class PhotoProfileFormComponent implements OnInit {
   photoFormProfile: FormGroup;
   files: Array<any> = [];
   progress = 0;
-  user: User;
   imageChangedEvent: any = '';
-  videoChangedEvent: any = '';
   croppedImage;
   cols;
   classSelectedCarousel;
   errorSubmitForm = '';
   spinner: boolean;
   spinnerFile: boolean;
-  eventCroop: Event;
   imageBase64String;
   currentIndex;
   user$:Observable<User>;
   hasImage:boolean = false;
 
   constructor(
-    private alertService: AlertService,
     private photoService: PhotoService,
     private formBuilder: FormBuilder,
-    private router: Router,
-    private activatedRoute: ActivatedRoute,
-    private securityCommons: SecurityCommonsService,
     public headerService: HeaderService,
     private postsService:PostsService,
     private userService:UserService
@@ -100,21 +89,12 @@ export class PhotoProfileFormComponent implements OnInit {
     });
     this.currentIndex = 0;
   }
-
   /** Cropped Events **/
   imageCropped(event: ImageCroppedEvent): any {
     this.croppedImage = event.base64;
     if(this.currentIndex !== undefined ){
       this.files[this.currentIndex].file = event.base64;
     }
-  }
-  cropperReady() {
-  }
-  imageLoaded(image: LoadedImage) {
-    // show cropper
-  }
-  loadImageFailed(): void {
-
   }
   items():any[]{
     return this.photoService.filters()
@@ -133,36 +113,6 @@ export class PhotoProfileFormComponent implements OnInit {
     }
     this.imageBase64String = item;
     this.currentIndex = index;
-  }
- 
-  /** Form Events **/
-  uploadVideo(file: File): any {
-    // this.classSelectedCarousel
-    this.photoService
-      .updatePhotoProfile(file)
-      .pipe(
-        finalize(() => {
-          this.router.navigate(['']);
-        }
-        )
-      )
-      .subscribe(
-        (event: HttpEvent<any>) => {
-
-          if (event.type === HttpEventType.UploadProgress) {
-
-            this.progress = Math.round(100 * event.loaded / event.total);
-
-          } else if (event.type === HttpEventType.Response) {
-            this.alertService.success('Upload complete');
-          }
-        },
-        err => {
-          this.alertService.danger('Failed to load the file, try later');
-        }
-      );
-    this.errorSubmitForm = '';
-    this.headerService.setCurrentSession('');
   }
   save(): any {
     this.spinner = true;
@@ -191,20 +141,6 @@ export class PhotoProfileFormComponent implements OnInit {
       this.errorSubmitForm = 'Insert a photo and a description';
       this.spinner = false;
     }
-  }
-  /** Helpers **/
-  uriToImage(url, callback){
-    var xhr = new XMLHttpRequest();
-    xhr.onload = function() {
-      var reader = new FileReader();
-      reader.onloadend = function() {
-        callback(reader.result);
-      }
-      reader.readAsDataURL(xhr.response);
-    };
-    xhr.open('GET', url);
-    xhr.responseType = 'blob';
-    xhr.send();
   }
   clickInput(){ 
     (<HTMLElement>document.querySelector('.file-input')).click();
