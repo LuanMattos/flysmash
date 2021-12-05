e894e666e1075a760524f832eef12f8787581281
patrick_mattos <patrick_mattos@terceiros.sicredi.com.br>
2021-12-04 21:23:36 -0300

    Correção delete dropdown photo list feed

--- src/app/photos/photo-list-feed/dropdown/dropdown.component.ts
+++ src/app/photos/photo-list-feed/dropdown/dropdown.component.ts
@@ -1,7 +1,7 @@
-import {Component, Input} from "@angular/core";
-import { timeStamp } from "console";
-import { PostsService } from "src/app/core/posts/posts.service";
-import { AlertService } from "src/app/shared/alert/alert.service";
+import {Component, Input} from '@angular/core';
+import { timeStamp } from 'console';
+import { PostsService } from 'src/app/core/posts/posts.service';
+import { AlertService } from 'src/app/shared/alert/alert.service';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -23,19 +23,24 @@ export class DropdownComponent{
       confirmButtonText: 'Yes',
       denyButtonText: `No`,
     }).then((result) => {
-      if (!result.isDenied) {
-        this.alertService.info("Deleting your post...",true);
+      console.log(result.isConfirmed)
+      if (result.isConfirmed) {
+        this.alertService.info('Deleting your post...',true);
         this.postsService
         .delete( this.post_id )
         .subscribe(
           (response) => {     
-            this.postsService.removePostsSubject(response);
-            this.alertService.success("Post was deleted");
+            this.postsService.removePostsSubject( response );
+            this.alertService.success('ost was deleted');
+            this.close();
           },
           err => {
-           this.alertService.warning("Error try again later");
+           this.alertService.warning('Error try again later');
+           this.close();
           }
         );
+      }else{
+        this.close();
       }
     });
   }
