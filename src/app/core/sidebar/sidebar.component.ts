a38dd0e6a0198bff2f6e106a736643424801e89a
patrick_mattos <patrick_mattos@terceiros.sicredi.com.br>
2021-12-07 16:03:23 -0300

    Retirar jquery do index e botar apenas no sidebar -SEO

--- src/app/core/sidebar/sidebar.component.ts
+++ src/app/core/sidebar/sidebar.component.ts
@@ -1,8 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { User } from '../user/user';
 import { UserService } from '../user/user.service';
+import $ from "jquery";
+
 
 
 @Component({
@@ -10,18 +12,32 @@ import { UserService } from '../user/user.service';
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent implements OnInit{
+export class SidebarComponent implements OnInit {
   form: FormGroup;
   showSidebar;
-  user:User;
+  user: User;
   $user: Observable<User>;
   constructor(
     private formBuilder: FormBuilder,
-    private userService:UserService
-    ) {}
+    private userService: UserService
+  ) { }
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.form = this.formBuilder.group({});
     this.$user = this.userService.getUser();
-  }    
+    this.functionsMenu();
+  }
+  functionsMenu(): void {
+    $('.sidebar_inner ul li a').on('click', function (e) {
+      if ($(this).closest("li").children("ul").length) {
+        if ($(this).closest("li").is(".active-submenu")) {
+          $('.sidebar_inner ul li').removeClass('active-submenu');
+        } else {
+          $('.sidebar_inner ul li').removeClass('active-submenu');
+          $(this).parent('li').addClass('active-submenu');
+        }
+        e.preventDefault();
+      }
+    });
+  }
 }
